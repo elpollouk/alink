3fe19493b64956499745fe8829a38cf0851b321a
Adrian O'Grady <elpollouk@users.noreply.github.com>
2020-12-05 21:24:01 +0000

    Raw serial data bytes in hex dumped to the screen as they're received

--- alink/alink.ino
+++ alink/alink.ino
@@ -1,20 +1,51 @@
 #include <LiquidCrystal.h>
 
-#define BUFFER_SIZE 16
+#define VERSION "0.1"
+#define MESSAGE_BUFFER_SIZE 16
+#define DATA_BUFFER_SIZE 8
 #define PORT_SPEED 115200
 
 // initialize the library with the numbers of the interface pins
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
-char message_buffer[BUFFER_SIZE + 1];
+char message_buffer[MESSAGE_BUFFER_SIZE + 1];
+uint8_t data_buffer[DATA_BUFFER_SIZE];
 int message_index = 0;
+int data_index = 0;
 bool clear_message = false;
 bool update_message = false;
 
+
+void appendMessageBuffer(char data) {
+  if (message_index >= MESSAGE_BUFFER_SIZE) return;
+  message_buffer[message_index++] = data;
+  message_buffer[message_index] = 0;
+  update_message = true;
+}
+
+char nibbleToHex(uint8_t nibble) {
+  if (nibble < 10) {
+    return '0' + nibble;
+  }
+  return 'A' + (nibble - 10);
+}
+
+void appendDataBuffer(char data) {
+  data_buffer[data_index++ % DATA_BUFFER_SIZE] = data;
+  
+  message_index = 0;
+  for (int i = 0; i < DATA_BUFFER_SIZE; i++) {
+    uint8_t byte = data_buffer[(data_index + i) % DATA_BUFFER_SIZE];
+    appendMessageBuffer(nibbleToHex(byte >> 4));
+    appendMessageBuffer(nibbleToHex(byte & 0xF));
+  }
+}
+
 void setup() {
-  memset(message_buffer, 0, BUFFER_SIZE + 1);
+  memset(message_buffer, 0, MESSAGE_BUFFER_SIZE + 1);
+  memset(data_buffer, 0, DATA_BUFFER_SIZE);
   lcd.begin(16, 2);
-  lcd.print("aLink 0.1 - ");
+  lcd.print("aLink " VERSION " - ");
 
   Serial.begin(PORT_SPEED);
 }
@@ -23,19 +54,14 @@ void loop() {
   // Receive data into buffer
   while (Serial.available()) {
     char inChar = (char)Serial.read();
-    switch (inChar) {
-      case '\n':
-        message_index = 0;
-        clear_message = true;
-        break;
-
-      default:
-        if (message_index >= BUFFER_SIZE) break;
-        message_buffer[message_index++] = inChar;
-        message_buffer[message_index] = 0;
-        update_message = true;
-        break;
+    /*if  (inChar == '\n') {
+      message_index = 0;
+      clear_message = true;
     }
+    else {
+      appendMessageBuffer(inChar);
+    }*/
+    appendDataBuffer(inChar);
   }
 
   // Display time
